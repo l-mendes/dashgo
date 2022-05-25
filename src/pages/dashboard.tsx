8a6430a3d196c7cb6a21362cdd984b73fafa2e1a
Lucas Mendes Madruga <lucas.madruga@kymo.it>
2022-05-24 22:04:29 -0300

    Layout

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -67,9 +67,9 @@ export default function Dashboard() {
       <Flex w="100%" my={6} maxW={1480} mx="auto" px={6}>
         <Sidebar />
 
-        <SimpleGrid flex='1' gap='4' minChildWidth='320px' align='flex-start'>
+        <SimpleGrid flex='1' gap='4' minChildWidth='320px' alignItems="flex-start">
           <Box
-            p='8'
+            p={['6', '8']}
             bg='gray.800'
             borderRadius={8}
             pb='4'
@@ -78,7 +78,7 @@ export default function Dashboard() {
             <Chart options={options} series={series} type='area' height={160}/>
           </Box>
           <Box
-            p='8'
+            p={['6', '8']}
             bg='gray.800'
             borderRadius={8}
             pb='4'
