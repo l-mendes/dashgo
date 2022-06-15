ebbe09ebc94a6086d8dd01d9c631e5bd02f0149d
Lucas Mendes Madruga <lucas.madruga@kymo.it>
2022-06-15 09:26:10 -0300

    Modulo IV concluido

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,15 +1,34 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react';
-import Link from 'next/link';
-import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import { useState } from 'react';
+import NextLink from 'next/link';
+import { RiAddLine } from 'react-icons/ri';
+import { Box, Button, Checkbox, Flex, Heading, Icon, Link, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react';
+
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
+import { useUsers } from '../../services/hooks/useUsers';
+import { queryClient } from '../../services/queryClient';
+import { api } from '../../services/api';
+
 export default function UserList() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(currentPage);
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
   });
+
+  async function handlePrefetchUser(userId: string) {
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const { data } = await api.get(`/users/${userId}`);
+
+      return data;
+    }, {
+      staleTime: 1000 * 60 * 10 // 10 minutos
+    });
+  }
 
   return (
     <Box>
@@ -22,9 +41,13 @@ export default function UserList() {
           <Flex mb='8' justify='space-between' align='center'>
             <Heading size='lg' fontWeight='normal'>
               Usuários
+
+              {!isLoading && isFetching && (
+                <Spinner size='sm' color='gray.500' marginLeft={4} />
+              )}
             </Heading>
 
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as='a'
                 size='sm'
@@ -34,36 +57,56 @@ export default function UserList() {
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
 
-          <Table colorScheme='whiteAlpha'>
-            <Thead>
-              <Tr>
-                <Th px={[4, 4, 6]} color='gray.300' width='8'>
-                  <Checkbox colorScheme='pink' />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={[4, 4, 6]}>
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Lucas Mendes</Text>
-                    <Text fontSize='sm' color='gray.300'>luucas.mds@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>12 de Abril, 2022</Td>}
-              </Tr>
-            </Tbody>
-          </Table>
+          { isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify='center'>
+              <Text>Falha ao obter dados do usuário.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme='whiteAlpha'>
+                <Thead>
+                  <Tr>
+                    <Th px={[4, 4, 6]} color='gray.300' width='8'>
+                      <Checkbox colorScheme='pink' />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.users.map(user => (
+                    <Tr key={user.id}>
+                      <Td px={[4, 4, 6]}>
+                        <Checkbox colorScheme='pink' />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Link color='purple.400' onMouseEnter={() => handlePrefetchUser(user.id)}>
+                            <Text fontWeight='bold'>{user.name}</Text>
+                          </Link>
+                          <Text fontSize='sm' color='gray.300'>{user.email}</Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination
+                totalCountOfRegisters={data.totalCount}
+                currentPage={currentPage}
+                onPageChange={setCurrentPage}
+              />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
